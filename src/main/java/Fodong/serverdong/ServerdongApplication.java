3556117c24cbd7fb59625b2b9f29e189191e3bc6
Chaeeun Kang <codms7020@naver.com>
2023-09-16 07:50:40 +0900

    [Setting] #1 - 프로젝트 초기 설정

---
+++ src/main/java/Fodong/serverdong/ServerdongApplication.java
@@ -0,0 +1,13 @@
+package Fodong.serverdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerdongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fodong/serverdong/ServerdongApplicationTests.java
@@ -0,0 +1,13 @@
+package Fodong.serverdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerdongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
