f0bd79911e775b29edaba4adf9ae5eae1b078511
Chaeeun Kang <codms7020@naver.com>
2023-09-18 16:44:31 +0900

    [Feat] #10 - RestaurantService 생성

---
+++ src/main/java/Fodong/serverdong/domain/restaurant/service/RestaurantService.java
@@ -0,0 +1,26 @@
+package Fodong.serverdong.domain.restaurant.service;
+
+import Fodong.serverdong.domain.restaurant.dto.response.ResponseRandomRestaurantDto;
+import Fodong.serverdong.domain.restaurant.repository.RestaurantQueryRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantQueryRepositoryImpl restaurantQueryRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<ResponseRandomRestaurantDto> getRandomRestaurant() {
+
+        return restaurantQueryRepository.getRandomRestaurant();
+
+
+    }
+}
