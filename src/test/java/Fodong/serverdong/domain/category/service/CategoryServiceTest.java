7f1219b23dc6fb8ffc49519da13efc4246183af2
Chaeeun Kang <codms7020@naver.com>
2023-09-25 16:55:37 +0900

    [Feat] #24 - 카테고리 리스트 조회 테스트 코드

---
+++ src/test/java/Fodong/serverdong/domain/category/service/CategoryServiceTest.java
@@ -0,0 +1,34 @@
+package Fodong.serverdong.domain.category.service;
+
+import Fodong.serverdong.domain.category.dto.response.ResponseCategoryListDto;
+import Fodong.serverdong.domain.category.repository.CategoryQueryRepositoryImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@SpringBootTest
+@Slf4j
+@Transactional
+class CategoryServiceTest {
+
+    @Autowired
+    CategoryQueryRepositoryImpl categoryQueryRepository;
+
+    @Test
+    @DisplayName("카테고리 리스트 조회")
+    void CategoryList(){
+        List<ResponseCategoryListDto> categoryList = categoryQueryRepository.getCategoryList();
+
+        for(ResponseCategoryListDto category : categoryList){
+            log.info(String.valueOf(category.getId()));
+            log.info(category.getCategoryName());
+            log.info(category.getCategoryImg());
+    }
+    }
+}
