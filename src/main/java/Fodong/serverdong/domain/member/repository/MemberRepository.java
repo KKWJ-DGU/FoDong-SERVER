62b8db8e0701465ad6fa8024e1b0e39adc963b98
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-02 00:32:12 +0900

    [Feat] #11 - MemberRepository 생성

---
+++ src/main/java/Fodong/serverdong/domain/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package Fodong.serverdong.domain.member.repository;
+
+
+import Fodong.serverdong.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
