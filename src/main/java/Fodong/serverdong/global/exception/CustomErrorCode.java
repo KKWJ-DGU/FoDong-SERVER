54f716cad1d000008f6067c7c181eb01dc259b60
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-04 13:46:21 +0900

    [Fix] #11 - CustomErrorCode 충돌 수정

--- src/main/java/Fodong/serverdong/global/exception/CustomErrorCode.java
+++ src/main/java/Fodong/serverdong/global/exception/CustomErrorCode.java
@@ -13,6 +13,8 @@ public enum CustomErrorCode {
 
     SUCCESS(OK,"success"),
     RESTAURANT_NOT_FOUND(BAD_REQUEST,"존재하지 않는 식당 정보 입니다."),
+    CATEGORY_NOT_CONTAIN(BAD_REQUEST,"존재하지 않는 카테고리ID가 포함되어 있습니다."),
+    CATEGORY_NOT_FOUND(BAD_REQUEST,"존재하지 않는 카테고리 입니다."),
     MEMBER_NOT_FOUND(BAD_REQUEST, "해당 회원이 존재하지 않습니다."),
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
