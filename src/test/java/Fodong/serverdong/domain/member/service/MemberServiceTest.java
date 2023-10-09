bb4ff3e8d9f62ae6b04bdcec28a3707a750e63ed
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-09 16:18:14 +0900

    [Feat] #36 - 회원 닉네임 설정 테스트 코드

---
+++ src/test/java/Fodong/serverdong/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package Fodong.serverdong.domain.member.service;
+
+import Fodong.serverdong.domain.member.Member;
+import Fodong.serverdong.domain.member.repository.MemberRepository;
+import Fodong.serverdong.global.exception.CustomErrorCode;
+import Fodong.serverdong.global.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    private final String testEmail = "testEmail@example.com";
+
+    @BeforeEach
+    void setup() {
+        Member newTestMember = Member.builder()
+                .email(testEmail)
+                .nickname(testEmail)
+                .build();
+        memberRepository.save(newTestMember);
+    }
+
+    @Test
+    @DisplayName("닉네임 설정")
+    void setNicknameTest() {
+        String newNickname = "시냥";
+
+        // 닉네임 설정
+        memberService.setNickname(testEmail, newNickname);
+
+        Member updatedMember = memberRepository.findByEmail(testEmail).orElse(null);
+        assertThat(updatedMember).isNotNull();
+        assertThat(updatedMember.getNickname()).isEqualTo(newNickname);
+    }
+
+    @Test
+    @DisplayName("닉네임 중복시 예외 발생")
+    void setDuplicateNicknameTest() {
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            memberService.setNickname(testEmail, testEmail);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo(new CustomException(CustomErrorCode.MEMBER_DUPLICATED_NICKNAME).getMessage());
+    }
+}
