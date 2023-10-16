e5202ee8adea93861881f01b22f2866b5c0948fd
Woo-Shinyoung <tlsdud8205@naver.com>
2023-10-16 21:39:45 +0900

    [Feat] #54 - 위시리스트 레포지토리 생성

---
+++ src/main/java/Fodong/serverdong/domain/wishlist/repository/WishlistRepository.java
@@ -0,0 +1,9 @@
+package Fodong.serverdong.domain.wishlist.repository;
+
+import Fodong.serverdong.domain.wishlist.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+
+}
