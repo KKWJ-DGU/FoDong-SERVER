1cd734856ed4740231d7524351645572827da8bf
Chaeniiiii <kce2360@gmail.com>
2023-10-19 16:06:09 +0900

    [Setting] #62 - Swagger 토큰 버튼 생성

--- src/main/java/Fodong/serverdong/global/config/SwaggerConfig.java
+++ src/main/java/Fodong/serverdong/global/config/SwaggerConfig.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.json.HTTP;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.http.HttpHeaders;
 import java.security.Security;
 
 
@@ -32,26 +34,33 @@ public class SwaggerConfig {
                 .description("Fodong 프로젝트 API 명세서");
 
         String access = "Access Token";
-        SecurityRequirement accessToken = new SecurityRequirement().addList(access);
-        Components components = new Components().addSecuritySchemes(access, new SecurityScheme()
-                .name(access)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("Authorization"));
-
         String refresh = "Refresh Token";
-        SecurityRequirement refreshToken = new SecurityRequirement().addList(refresh);
-        Components components1 = new Components().addSecuritySchemes(refresh, new SecurityScheme()
-                .name(refresh)
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(access)
+                .addList(refresh);
+
+        SecurityScheme accessTokenSchema = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
-                .bearerFormat("Refresh"));
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+
+        SecurityScheme refreshTokenScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Refresh");
+
+        Components components = new Components()
+                .addSecuritySchemes(access,accessTokenSchema)
+                .addSecuritySchemes(refresh,refreshTokenScheme);
+
 
         return new OpenAPI()
                 .components(components)
-                .addSecurityItem(accessToken)
-                .components(components1)
-                .addSecurityItem(refreshToken)
+                .addSecurityItem(securityRequirement)
                 .info(info);
     }
 
