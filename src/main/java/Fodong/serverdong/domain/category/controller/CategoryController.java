a4e1c19cd7fc66f93ce03836e5661be4fa969862
Chaeniiiii <kce2360@gmail.com>
2023-10-21 21:09:56 +0900

    [Setting] #62 - CategoryController 토큰 파라미터 생성

--- src/main/java/Fodong/serverdong/domain/category/controller/CategoryController.java
+++ src/main/java/Fodong/serverdong/domain/category/controller/CategoryController.java
@@ -1,12 +1,13 @@
 package Fodong.serverdong.domain.category.controller;
 
-import Fodong.serverdong.domain.category.Category;
+
 import Fodong.serverdong.domain.category.dto.response.ResponseCategoryListDto;
 import Fodong.serverdong.domain.category.service.CategoryService;
 import Fodong.serverdong.global.config.ApiDocumentResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class CategoryController {
     @ApiDocumentResponse
     @Operation( summary = "카테고리 리스트", description = "카테고리 리스트를 조회합니다.")
     @GetMapping("/list")
-    public List<ResponseCategoryListDto> getCategoryList(){
+    public List<ResponseCategoryListDto> getCategoryList(@RequestHeader("Authorization") String authorization){
         return categoryService.getCategoryList();
     }
 }
