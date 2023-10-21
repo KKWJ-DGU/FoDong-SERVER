0a94e2fde92ce242814b844a6b0eed4fb3cabc3b
Chaeniiiii <kce2360@gmail.com>
2023-10-22 03:03:38 +0900

    [Setting] #62 - Swagger 문서 토큰별 그룹화 설정

--- src/main/java/Fodong/serverdong/global/config/SwaggerConfig.java
+++ src/main/java/Fodong/serverdong/global/config/SwaggerConfig.java
@@ -4,20 +4,39 @@ package Fodong.serverdong.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi fodongApi(){
+    public GroupedOpenApi onlyAccessTokenApi(){
         return GroupedOpenApi.builder()
-                .group("fodong-api")
+                .group("AccessToken API")
+                .addOpenApiCustomiser(accessNeed())
                 .pathsToMatch("/**")
+                .pathsToExclude("/api/member/login/**" , "/api/membertoken/reissue")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi TokenApi(){
+        return GroupedOpenApi.builder()
+                .group("AllToken API")
+                .addOpenApiCustomiser(refreshNeed())
+                .pathsToMatch("/api/membertoken/reissue")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi NonTokenApi(){
+        return GroupedOpenApi.builder()
+                .group("NonToken API")
+                .pathsToMatch("/api/member/login/**")
                 .build();
     }
 
@@ -34,4 +53,32 @@ public class SwaggerConfig {
                 .info(info);
     }
 
+    private SecurityScheme accessTokenSchema(){
+        return new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+    public OpenApiCustomiser accessNeed() {
+
+        return OpenApi -> OpenApi
+                .addSecurityItem(new SecurityRequirement().addList("AccessToken"))
+                .getComponents().addSecuritySchemes("AccessToken", accessTokenSchema());
+    }
+
+    public OpenApiCustomiser refreshNeed() {
+        SecurityScheme refreshTokenSchema = new SecurityScheme()
+                .name("Refresh")
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+
+        return OpenApi -> OpenApi
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .addSecurityItem(new SecurityRequirement().addList("Refresh"))
+                .getComponents().addSecuritySchemes("Authorization",accessTokenSchema()).addSecuritySchemes("Refresh",refreshTokenSchema);
+    }
 }
