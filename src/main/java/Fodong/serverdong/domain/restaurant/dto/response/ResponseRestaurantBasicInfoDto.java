7f9e9fd93627b7b4f39cd87e904281146ca15d4b
Chaeniiiii <kce2360@gmail.com>
2023-12-01 22:52:50 +0900

    [Refactor] #18 - 식당 기본 정보 DTO 수정

--- src/main/java/Fodong/serverdong/domain/restaurant/dto/response/ResponseRestaurantBasicInfoDto.java
+++ src/main/java/Fodong/serverdong/domain/restaurant/dto/response/ResponseRestaurantBasicInfoDto.java
@@ -3,19 +3,15 @@ package Fodong.serverdong.domain.restaurant.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Getter
-public class ResponseRestaurantInfoDto {
+public class ResponseRestaurantBasicInfoDto {
 
     private String name;
     private String address;
     private String imgUrl;
-    private String categoryName;
     private String phoneNumber;
     private String webUrl;
-    private List<ResponseMenuInfo> menuInfoList;
     private int wishCount;
     private Boolean wishState;
 
