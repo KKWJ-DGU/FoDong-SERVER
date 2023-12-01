893f524b23c772df8e5e0599c043924df5518aca
Chaeniiiii <kce2360@gmail.com>
2023-12-01 22:47:02 +0900

    [Feat] #18 - 식당 정보 DTO 생성 (카테고리 리스트, 메뉴 리스트 추가)

---
+++ src/main/java/Fodong/serverdong/domain/restaurant/dto/response/ResponseRestaurantAllInfoDto.java
@@ -0,0 +1,35 @@
+package Fodong.serverdong.domain.restaurant.dto.response;
+
+import Fodong.serverdong.domain.category.dto.response.ResponseCategoryInfoListDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class ResponseRestaurantAllInfoDto {
+
+    private String name;
+    private String address;
+    private String imgUrl;
+    private String phoneNumber;
+    private String webUrl;
+    private List<ResponseCategoryInfoListDto> categoryInfoList;
+    private List<ResponseMenuInfoListDto> menuInfoList;
+    private int wishCount;
+    private Boolean wishState;
+
+    public ResponseRestaurantAllInfoDto(List<ResponseRestaurantBasicInfoDto> responseRestaurantBasicInfoDto,List<ResponseMenuInfoListDto> responseMenuInfoListDto, List<ResponseCategoryInfoListDto> responseCategoryInfoListDto){
+        this.name= responseRestaurantBasicInfoDto.get(0).getName();
+        this.address= responseRestaurantBasicInfoDto.get(0).getAddress();
+        this.imgUrl= responseRestaurantBasicInfoDto.get(0).getImgUrl();
+        this.phoneNumber= responseRestaurantBasicInfoDto.get(0).getPhoneNumber();
+        this.webUrl= responseRestaurantBasicInfoDto.get(0).getWebUrl();
+        this.categoryInfoList= responseCategoryInfoListDto;
+        this.menuInfoList= responseMenuInfoListDto;
+        this.wishCount= responseRestaurantBasicInfoDto.get(0).getWishCount();
+        this.wishState= responseRestaurantBasicInfoDto.get(0).getWishState();
+    }
+
+}
